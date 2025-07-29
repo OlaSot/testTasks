9de455ee964e5bd0ba5b41eb8fad4964a2faba2b
Olha Sotova <sotovaola21@gmail.com>
2025-07-30 00:55:08 +0200

    route has been changed

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,15 @@ function App() {
             <main className="flex-grow">
               <Routes>
                 {/* Public Routes */}
-                <Route path="/" element={<Landing />} />
+                <Route
+  path="/"
+  element={
+    <ProtectedRoute>
+      <Landing />
+    </ProtectedRoute>
+  }
+/>
+
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
